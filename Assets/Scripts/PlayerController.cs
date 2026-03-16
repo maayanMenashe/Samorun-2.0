52fe3008de4a7d3e5686a1bce8837385fc7f487f
maayanMenashe <maayanreal@gmail.com>
2026-03-16 03:20:42 +0200

    player and enemy death
    enemies get destroyed when they are killed
    
    player gets sent back and relevant things reset when too close to an enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,22 @@
 using System;
 using UnityEngine;
+using System.Collections;
+
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 startingPosition;
+    private Vector3 respawnPoint;
+    private Camera Camera;
+    private float originalCameraSize;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startingPosition = transform.position;
+        respawnPoint = startingPosition;
+        Camera = Camera.main;
+        originalCameraSize = Camera.orthographicSize;
     }
 
     // Update is called once per frame
@@ -16,5 +26,24 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            transform.position = respawnPoint;
+            Camera.orthographicSize = originalCameraSize;
+            GetComponent<CharacterMovementController>().speed = 10;
+            Time.timeScale = 1f;
 
+            GameObject[] allGameObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+            foreach (var gameObject in allGameObjects)
+            {
+                if (gameObject.CompareTag("Enemy"))
+                {
+                    Destroy(gameObject);
+                }
+                
+            }
+        }
+    }
 }

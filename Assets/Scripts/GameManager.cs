8012cac2ad516be4c2249a4f8e64cb1944eeb57c
maayanMenashe <maayanreal@gmail.com>
2026-03-17 08:15:01 +0200

    added a main menu in a very lazy way
    no time or knowledge for scene manager so i did this

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,23 +6,32 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject pauseMenu;
     
+    [SerializeField]
+    private GameObject mainMenu;
+    
     private AudioManager audioManager;
 
     private bool isPaused;
+    
+    private bool gameStarted;
 
     private float currentTimeScale = 1f;
+    
+    
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioManager = FindAnyObjectByType<AudioManager>();
+        Time.timeScale = 0f;
+        audioManager.backgroundMusic.volume = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Pause"))
+        if (Input.GetButtonDown("Pause") && gameStarted)
         {
             OpenOrClosePauseMenu();
         }
@@ -30,7 +39,6 @@ public class GameManager : MonoBehaviour
 
     public void OpenOrClosePauseMenu()
     {
-        
         pauseMenu.SetActive(!isPaused);
         isPaused = !isPaused;
         if (isPaused)
@@ -47,6 +55,17 @@ public class GameManager : MonoBehaviour
         
         
     }
+
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        mainMenu.SetActive(false);
+        Time.timeScale = currentTimeScale;
+        audioManager.backgroundMusic.volume = 0.3f;
+    }
+    
+    
     
     public void QuitGame()
     {
